27878a28b707770800e629e364f84ffec57ef9e8
Alexandr Ricov <rykovalexandr@gmail.com>
2020-11-30 13:37:36 +0200

    init from scratch

---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <header>
+        <svg width="150" height="28">
+          <use xlinkHref="/sprite.svg#logo" />
+        </svg>
+      </header>
+    </>
+  );
+}
